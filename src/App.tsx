6b0850f69c463a21b2e509034799c1f5a030ffe2
Akhil <akhilvijayan004@gmail.com>
2021-05-31 19:21:10 +1000

    create components and set routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from "./components/home";
+import Table from './components/table';
+import PageNotFound from './components/pageNotFound';
 
 function App() {
     return (
-        <div className="App">
-            <h1>Project</h1>
-        </div>
+        <Router>
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/table" component={Table} />
+                <Route component={PageNotFound} />
+            </Switch>
+        </Router>
     );
 }
 
