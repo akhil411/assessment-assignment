1b097a04cc190093b83349b5e8bf1ba07d19a943
Akhil <akhilvijayan004@gmail.com>
2021-05-31 20:09:14 +1000

    display results into table

--- src/components/table/index.tsx
+++ src/components/table/index.tsx
@@ -1,12 +1,109 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { connect } from 'react-redux';
+import { setTableData } from './../../redux/actions';
+import { History } from 'history';
 import './table.css';
 
-const Table = () => {
+type Result = {
+    country: string;
+    name: string;
+    alpha_two_code: string;
+    [key: string]: string;
+}
+
+type Props = {
+    tableData: Result[];
+    history: History;
+    setTableData: typeof setTableData
+}
+
+type TableReducer = {
+    tableData: Result[];
+    tableDataError: string;
+}
+
+type ReduxState = {
+    tableReducer: TableReducer
+}
+
+const Table: React.FC<Props> = ({ tableData, history, setTableData }) => {
+    const [results, setResults] = useState<Result[]>([]);
+
+    useEffect(() => {
+        setResults(tableData);
+    }, [tableData]);
+
+    function handleValueChange(event: React.ChangeEvent<HTMLInputElement>) {
+        const { name, value } = event.target;
+        const key = Number(event.target.getAttribute('data-index'));
+        let newResults: Result[] = [...results];
+        newResults[key][name] = value;
+        setTableData(newResults);
+    }
+
+    function handleButtonClick() {
+        history.push('/');
+    }
+
     return (
         <div>
             <h1>Table</h1>
+            <button onClick={handleButtonClick}>Back</button>
+            {results ? (
+                <table className="styled-table">
+                    <thead>
+                        <tr>
+                            <th>Country</th>
+                            <th>Code</th>
+                            <th>Name</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        { results.map((result: Result, index: number) => (
+                            <tr key={index}>
+                                <td>
+                                    <input 
+                                        type="text" 
+                                        data-index={index} 
+                                        name="country" 
+                                        value={result.country} 
+                                        onChange={handleValueChange}
+                                    />
+                                </td>
+                                <td>
+                                    <input 
+                                        type="text" 
+                                        data-index={index} 
+                                        name="alpha_two_code" 
+                                        value={result.alpha_two_code} 
+                                        onChange={handleValueChange}
+                                    />
+                                </td>
+                                <td>
+                                    <input 
+                                        type="text" 
+                                        data-index={index} 
+                                        name="name" 
+                                        value={result.name} 
+                                        onChange={handleValueChange}
+                                    />
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            ) : null }
         </div>
     )
 }
 
-export default Table
+const mapDispatchToProps = {
+    setTableData,
+};
+
+const mapStateToProps = (state: ReduxState) => ({
+    tableData: state.tableReducer.tableData,
+    dataError: state.tableReducer.tableDataError
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Table);
--- src/redux/actions/index.tsx
+++ src/redux/actions/index.tsx
@@ -1,3 +1,15 @@
+type Result = {
+    country: string;
+    name: string;
+    alpha_two_code: string;
+    [key: string]: string;
+}
+
 export const getTableData = () => ({
     type: 'GET_TABLE_DATA'
+});
+
+export const setTableData = (data: Array<Result>) => ({
+    type: 'SET_TABLE_DATA',
+    data: data
 });
